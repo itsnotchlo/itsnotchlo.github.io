c357d2f7117efd59397dc1a52ab4c25e9f7b98f0
itsnotchlo <youyinz1998@gmail.com>
2016-08-17 12:21:41 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -29,7 +29,7 @@
 
 var rules = {
   "~*x hello ~*y": [
-    "How do you do. Please state your problem."
+    "How are you, sweetheart?"
   ],
   "~*x computer ~*y": [
     "Do computers worry you?",
@@ -44,7 +44,7 @@ var rules = {
     "I am not interested in names",
   ],
   "~*x sorry ~*y": [
-    "Please don't apologize",
+    "Please don't apologize, hon!",
     "Apologies are not necessary",
     "What feelings do you have when you apologize",
   ],
